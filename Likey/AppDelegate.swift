f576c2b325dee0ca92ef38e5ab83c68aac443b6a
Julie Xiao <julixiao@gmail.com>
2019-03-24 13:44:06 -0400

    Initial Commit

---
+++ Likey/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Likey
+//
+//  Created by Julie  on 2019-03-24.
+//  Copyright © 2019 julixiao. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Likey/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Likey
+//
+//  Created by Julie  on 2019-03-24.
+//  Copyright © 2019 julixiao. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
