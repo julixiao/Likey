c0820cbf63412de75230ffe9ec053d1258587193
Julie Xiao <julixiao@gmail.com>
2020-02-05 00:07:58 -0500

    Adding entry fields + file location preference

---
+++ Likey/PrefsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsViewController.swift
+//  Likey
+//
+//  Created by Julie  on 2020-02-04.
+//  Copyright © 2020 julixiao. All rights reserved.
+//
+
+import Cocoa
+
+class SettingsViewController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
